b451bbb660cfab95581db6ad855f941d61364088
Arek <arkadiusz.zalewsky@gmail.com>
2017-02-07 21:43:32 +0100

    webpack-dev-server and babel
    Setting up Webpac-dev-server with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,24 @@
-const webpack = require("webpack")
-const autoprefixer = require('autoprefixer')
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
-    './app/App.jsx',
+    './src/App.jsx',
   ],
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/build/",
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
+    path: './build',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: ['react-hot', 'babel'],
-        loaders: [
-          'babel?presets[]=es2015,presets[]=stage-2,presets[]=react,plugins[]=transform-decorators-legacy,plugins[]=react-hot-loader/babel'
-        ]
-      },
-      {
-        test: /\.(css|scss)$/,
-        loaders: [
-          'style',
-          'css?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader',
-          'sass'
-        ]
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
     ]
-  },
-  postcss: [autoprefixer],
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  }
 };
