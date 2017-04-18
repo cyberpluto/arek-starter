f0acd1e8bc6ed4c4f6b11415d4fd183376e3ed90
Arek <arek@yaska.eu>
2017-04-18 19:01:05 +0200

    Styling button with styled components

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,16 +1,29 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {exampleAction} from 'Redux/ducks/example-duck.js'
+import styled from 'styled-components'
+
+const Button = styled.button`
+	background: rgb(145,202,73);
+	border: none;
+	border-radius: .5em;
+	color: #fff;
+	font-size: 1.5em;
+	padding: .5em;
+	&:focus {
+		outline: 0;
+	}
+`
 
 class Main extends Component {
 	render() {
 		const {test, exampleAction} = this.props
 		return (
-			<button
+			<Button
 				onClick={exampleAction}
 			>
 				Click: {test}
-			</button>
+			</Button>
 		)
 	}
 }
