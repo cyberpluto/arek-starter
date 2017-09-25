f84df9660ee88640f7b2bf0b29c20c2dfb0537b8
Arek <arek@yaska.eu>
2017-09-25 23:20:47 +0200

    Heroku-postbuild. Create 'dist' dir if doesn't exist.

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const sqlite3 = require('sqlite3').verbose()
 import {promisify} from 'bluebird'
+import fs from 'fs'
+
+const dir = './dist'
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir)
+}
 
 const db = new sqlite3.Database('./dist/arek-starter')
 db.get = promisify(db.get)
